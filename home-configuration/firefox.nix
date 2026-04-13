eb72b1dfa1384d8f2920fb5139a3895deebf2691
Finn <finn.schubert@stud.tu-darmstadt.de>
2026-04-13 15:27:46 +0200

    Firefox search engines

--- home-configuration/firefox.nix
+++ home-configuration/firefox.nix
@@ -17,6 +17,23 @@
             icon = "https://www.merriam-webster.com/favicon.svg";
             definedAliases = [ "!mw" ];
           };
+          wikipedia = {
+            name = "Wikipedia";
+            urls = [ { template = "https://en.wikipedia.org/w/index.php?search={searchTerms}"; } ];
+            icon = "https://en.wikipedia.org/static/favicon/wikipedia.ico";
+            definedAliases = [ "!wp" ];
+          };
+          rocq = {
+            name = "Rocq";
+            urls = [ { template = "https://rocq-prover.org/doc/V9.2.0/refman/search.html?q={searchTerms}"; } ];
+            icon = "https://rocq-prover.org/_/YTlhZTE1YTM5OGJkOGQ0MGQ0NDA2YjcwNmU4ODE3Yzc/logos/favicon.ico";
+            definedAliases = [ "!rocq" ];
+          };
+
+          ddg.metaData.alias = "!ddg"; # Duck Duck Go
+          bing.metaData.hidden = true;
+          ecosia.metaData.hidden = true;
+          perplexity.metaData.hidden = true;
         };
         force = true;
       };
