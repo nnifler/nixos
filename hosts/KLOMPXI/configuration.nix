ddc3aea23b46b66689d2edf06e7dbf23e79ca211
Finn <finn.schubert@stud.tu-darmstadt.de>
2026-08-05 18:08:21 +0200

    Add TUDA VPN

--- hosts/KLOMPXI/configuration.nix
+++ hosts/KLOMPXI/configuration.nix
@@ -15,10 +15,49 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "KLOMPXI"; # Define your hostname.
+  networking.hostName = "KLOMPXI";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = with pkgs; [ networkmanager-openconnect ];
+    ensureProfiles.profiles = {
+      "tuda-vpn" = {
+        connection = {
+          autoconnect = "false";
+          id = "tuda-vpn";
+          type = "vpn";
+        };
+        ipv4 = {
+          method = "auto";
+        };
+        ipv6 = {
+          addr-gen-mode = "stable-privacy";
+          method = "auto";
+        };
+        vpn = {
+          authtype = "password";
+          autoconnect-flags = "0";
+          certsigs-flags = "0";
+          cookie-flags = "2";
+          disable_udp = "no";
+          enable_csd_trojan = "no";
+          gateway = "vpn.hrz.tu-darmstadt.de";
+          gateway-flags = "2";
+          gwcert-flags = "2";
+          lasthost-flags = "0";
+          pem_passphrase_fsid = "no";
+          useragent = "AnyConnect";
+          protocol = "anyconnect";
+          resolve-flags = "2";
+          service-type = "org.freedesktop.NetworkManager.openconnect";
+          stoken_source = "disabled";
+          xmlconfig-flags = "0";
+          password-flags = "2";
+        };
+      };
+    };
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
